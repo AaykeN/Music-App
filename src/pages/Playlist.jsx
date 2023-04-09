5f4bcb5d0cef756be6beeeaa6602c7e3993ee83c
Arnond <van.suvannarat@gmail.com>
2023-04-09 18:14:31 +0800

    playlist-playPause-DONE

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -9,13 +9,13 @@ import { HiDotsVertical, HiOutlineHeart } from "react-icons/hi";
 const findPlaylist = (playlistId) => {
   const { data, isFetching, error } = useGetPlaylistQuery();
   const playlist = data?.find((p) => p.id === playlistId);
-  return { playlist, isFetching, error };
+  return { playlist, isFetching, error, data };
 };
 
 const Playlist = () => {
   const dispatch = useDispatch();
   const { playlistId } = useParams();
-  const { playlist, isFetching, error } = findPlaylist(playlistId);
+  const { playlist, isFetching, error, data } = findPlaylist(playlistId);
   const { activeSong, isPlaying } = useSelector((state) => state.player);
   const playListSongs = playlist?.files;
 
@@ -37,8 +37,11 @@ const Playlist = () => {
               <p>{playListSongs?.length} Songs ~ 16 hrs+</p>
             </div>
             <div className="flex gap-4 mt-5 lg:mt-14 flex-wrap md:flex-nowrap">
-              {playListButton.map((button) => (
-                <button className="bg-[#33373B]/[37%] hover:bg-[#2e323440] w-fit py-[10px] md:px-[20px] px-[15px] rounded-full text-start flex items-center gap-2 md:gap-3">
+              {playListButton.map((button, index) => (
+                <button
+                  key={index}
+                  className="bg-[#33373B]/[37%] hover:bg-[#2e323440] w-fit py-[10px] md:px-[20px] px-[15px] rounded-full text-start flex items-center gap-2 md:gap-3"
+                >
                   <img
                     src={button.icon}
                     alt={button.name}
@@ -54,43 +57,51 @@ const Playlist = () => {
         </div>
 
         <div className="flex flex-col gap-3 mt-5 lg:mt-16">
-          {playListSongs?.map((song) => (
-            <div
-              className="bg-[#33373B]/[37%] rounded-2xl flex py-2 px-3 md:p-3 items-center gap-4 md:gap-10 lg:gap-28 "
-              key={song.id}
-            >
-              <div className="flex items-center">
-                <img
-                  src={song.cover}
-                  alt="art cover"
-                  className="w-14 h-auto rounded-lg object-cover"
-                />
-                <div className="md:ml-3 lg:ml-5 hidden md:block">
-                  <HiOutlineHeart className="w-6 h-auto" />
+          {playListSongs?.map((song, i) => {
+            const handlePlayClick = () => {
+              dispatch(setActiveSong({ song, data, i }));
+              dispatch(playPause(true));
+            };
+
+            return (
+              <div
+                onClick={handlePlayClick}
+                className="bg-[#33373B]/[37%] hover:bg-[#2e323440] cursor-pointer rounded-2xl flex py-2 px-3 md:p-3 items-center gap-4 md:gap-10 lg:gap-28 "
+                key={song.id}
+              >
+                <div className="flex items-center">
+                  <img
+                    src={song.cover}
+                    alt="art cover"
+                    className="w-14 h-auto rounded-lg object-cover"
+                  />
+                  <div className="md:ml-3 lg:ml-5 hidden md:block">
+                    <HiOutlineHeart className="w-6 h-auto" />
+                  </div>
                 </div>
-              </div>
-              <div className="flex w-full md:items-center text-sm lg:text-base font-light">
-                <div className="md:flex-1 flex-1 flex flex-col md:flex-row md:items-center shrink-0 gap-1 md:gap-0">
-                  <p className=" flex-1 shrink-0 text-start">
-                    {song.title} - {song.artist}
-                  </p>
-                  <p className=" flex-1 text-start md:text-center text-xs md:text-base">
-                    Single
-                  </p>
-                </div>
-                <div className="md:flex-1 lg:flex-1 flex flex-col-reverse md:flex-row shrink gap-1 md:gap-0 items-end md:items-center ">
-                  <div className="flex-1 flex justify-center">
-                    <p className="text-end md:text-center text-xs md:text-base">
-                      {song.duration}
+                <div className="flex w-full md:items-center text-sm lg:text-base font-light">
+                  <div className="flex-1 flex flex-col min-w-0 w-40  md:flex-row md:items-center gap-1 md:gap-0">
+                    <p className="truncate flex-1 text-start">
+                      {song.title} - {song.artist}
+                    </p>
+                    <p className=" flex-1 text-start md:text-center text-xs md:text-base">
+                      Single
                     </p>
                   </div>
-                  <div className="flex-1 flex flex-shrink-5 justify-end items-center">
-                    <HiDotsVertical className="lg:mr-6 text-[#FACD66]" />
+                  <div className="md:flex-1 lg:flex-1 flex flex-col-reverse md:flex-row shrink gap-1 md:gap-0 items-end md:items-center ">
+                    <div className="flex-1 flex justify-center">
+                      <p className="text-end md:text-center text-xs md:text-base">
+                        {song.duration}
+                      </p>
+                    </div>
+                    <div className="flex-1 flex flex-shrink-5 justify-end items-center">
+                      <HiDotsVertical className="lg:mr-6 text-[#FACD66]" />
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     </>
