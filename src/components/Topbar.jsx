a256b9019a74b44523b9219c03bc1de32e0820dd
Arnond <van.suvannarat@gmail.com>
2023-04-20 17:00:36 +0800

    added-bgImage-playlist

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -5,7 +5,7 @@ import logo from "../assets/img/logo.svg";
 const Topbar = () => {
   return (
     <>
-      <div className="px-9 h-[75px] sticky top-0 z-20 items-center bg-[#1d2123] hover:border-gray-100 scroll flex md:justify-start justify-end">
+      <div className="px-9 h-[75px] sticky top-0 z-20 items-center bg-transparent hover:border-gray-100 scroll flex md:justify-start justify-end">
         <Searchbar />
       </div>
     </>
--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -37,62 +37,69 @@ const Playlist = () => {
     dispatch(setActiveSong({ song, data: playlist.files, i }));
     dispatch(playPause(true));
   };
-
   const totalTime = calculateTotalTime(playListSongs);
 
   return (
     <>
-      <div className="text-white text-xl px-7 pb-[130px] container-padding ">
-        <div className="lg:flex lg:gap-8 lg:items-end">
-          <img
-            src={playlist?.cover}
-            alt="cover_art"
-            className="rounded-[50px] w-full h-auto aspect-square sm:w-[370px] sm:h-[370px] object-cover object-center "
-          />
-          <div className="mt-7 lg:mt-0">
-            <h2 className="text-[#A4C7C6] text-4xl font-bold mb-3">
-              {playlist?.title}
-            </h2>
-            <div className="text-base text-white/90 font-light lg:max-w-[700px]">
-              <p className="mb-3">{playlist?.info}</p>
-              <p>
-                {playListSongs?.length} Songs ~ {totalTime.minutes} mins{" "}
-                {totalTime.seconds} secs
-              </p>
-            </div>
-            <div className="flex gap-4 mt-5 lg:mt-14 flex-wrap md:flex-nowrap">
-              <PlayAllBtn
-                handleClick={() => handlePlayClick(playlist.files[0], 0)}
-                playALlClass="bg-[#33373B]/[37%] hover:bg-[#2e323440] w-fit py-[10px] md:px-[20px] px-[15px] rounded-full text-start flex items-center gap-2 md:gap-3"
-              />
-              <AddCollectionBtn playlist={playlist} />
+      <div className="bg-gradient-to-t from-[#1d2123] to-[#1d21232c] absolute z-[-2] -left-0 -top-0 max-w-[1920px] w-[100%] h-[100vh]" />
+      <img
+        src={playlist?.cover}
+        alt="bg-cover"
+        className="absolute z-[-3] opacity-[0.07] -left-0 -top-0 w-[100vw] h-[100vh] object-cover object-center"
+      />
+      <div>
+        <div className="text-white text-xl px-7 pb-[130px] container-padding">
+          <div className="lg:flex lg:gap-8 lg:items-end">
+            <img
+              src={playlist?.cover}
+              alt="cover_art"
+              className="rounded-[50px] w-full h-auto aspect-square sm:w-[370px] sm:h-[370px] object-cover object-center "
+            />
+            <div className="mt-7 lg:mt-0">
+              <h2 className="text-[#A4C7C6] text-4xl font-bold mb-3">
+                {playlist?.title}
+              </h2>
+              <div className="text-base text-white/90 font-light lg:max-w-[700px]">
+                <p className="mb-3">{playlist?.info}</p>
+                <p>
+                  {playListSongs?.length} Songs ~ {totalTime.minutes} mins{" "}
+                  {totalTime.seconds} secs
+                </p>
+              </div>
+              <div className="flex gap-4 mt-5 lg:mt-14 flex-wrap md:flex-nowrap">
+                <PlayAllBtn
+                  handleClick={() => handlePlayClick(playlist.files[0], 0)}
+                  playALlClass="bg-[#33373B]/[37%] hover:bg-[#2e323440] w-fit py-[10px] md:px-[20px] px-[15px] rounded-full text-start flex items-center gap-2 md:gap-3"
+                />
+                <AddCollectionBtn playlist={playlist} />
 
-              <LikeButton
-                likeClass="bg-[#33373B]/[37%] hover:bg-[#2e323440] w-fit py-[10px] md:px-[20px] px-[15px] lg:px-[10px] rounded-full text-start flex items-center gap-2 md:gap-3"
-                className="h-5 w-auto"
-                alt="Like"
-                playlist={playlist}
-              />
+                <LikeButton
+                  likeClass="bg-[#33373B]/[37%] hover:bg-[#2e323440] w-fit py-[10px] md:px-[20px] px-[15px] lg:px-[10px] rounded-full text-start flex items-center gap-2 md:gap-3"
+                  className="h-5 w-auto"
+                  alt="Like"
+                  playlist={playlist}
+                />
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="flex flex-col gap-3 mt-5 lg:mt-14">
-          {playListSongs?.map((song, i) => {
-            const handlePlayClick = () => {
-              dispatch(setActiveSong({ song, data: playlist.files, i }));
-              dispatch(playPause(true));
-            };
+          <div className="flex flex-col gap-3 mt-5 lg:mt-14">
+            {playListSongs?.map((song, i) => {
+              const handlePlayClick = () => {
+                dispatch(setActiveSong({ song, data: playlist.files, i }));
+                dispatch(playPause(true));
+              };
 
-            return (
-              <PlaylistSongCard
-                handlePlayClick={() => handlePlayClick(song, i)}
-                song={song}
-                key={`PlaylistSongCard-${i}`}
-              />
-            );
-          })}
-          {isFetching && <Loader className="h-100%" />}
+              return (
+                <PlaylistSongCard
+                  handlePlayClick={() => handlePlayClick(song, i)}
+                  song={song}
+                  key={`PlaylistSongCard-${i}`}
+                />
+              );
+            })}
+            {isFetching && <Loader className="h-100%" />}
+          </div>
         </div>
       </div>
     </>
@@ -100,3 +107,16 @@ const Playlist = () => {
 };
 
 export default Playlist;
+
+// style={{
+//   top: 0,
+//   backgroundRepeat: "no-repeat",
+//   opacity: 0.05,
+//   position: `absolute`,
+//   width: `100vw`,
+//   height: `100vh`,
+//   objectFit: `cover`,
+//   objectPosition: `center`,
+//   left: 0,
+//   backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0) 100%), url(${playlist?.cover})`,
+// }}
