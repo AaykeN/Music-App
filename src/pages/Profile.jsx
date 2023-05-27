a6c8688dd893082f3689e0828935c14522536afa
Arnond <van.suvannarat@gmail.com>
2023-05-27 11:21:40 +0800

    add link to my name

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,7 +5,13 @@ const Profile = () => {
     <div className="container-padding">
       <div className="text-white text-xl">Profile</div>
       <div className="flex w-full text-2xl text-white justify-center items-center h-[50vh]">
-        Built By Arnond 👋🏻
+        <p>
+          Built By{"  "}
+          <a href="https://arnond.com/" target="_blank" className="underline">
+            Arnond
+          </a>
+          👋🏻
+        </p>
       </div>
     </div>
   );
